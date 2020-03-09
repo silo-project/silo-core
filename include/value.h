157a01cebaaf7a818730f46764a7a7339f6e6c6a
Sangwon Ryu <bdboy090@naver.com>
2020-03-09 21:53:09 +0700

    파일 정리

--- include/value.h
+++ include/value.h
@@ -3,16 +3,10 @@
 #ifndef VALUE_H
 #define VALUE_H
 
-<<<<<<< Updated upstream
-struct Value {
-    uint8_t  size;
-    uint64_t value;
-=======
 struct value {
     uint8_t  size;
     uint64_t value;
     uint64_t highImpedance;
->>>>>>> Stashed changes
     uint64_t unknown;
 };
 
