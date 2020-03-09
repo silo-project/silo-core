3fd83c7621e8dc3a92bbd8153f67594e01ad50fc
Jiwoo Kim <kimseojin968@gmail.com>
2020-03-09 23:50:03 +0900

    value 구조체 추가

--- include/value.h
+++ include/value.h
@@ -3,9 +3,16 @@
 #ifndef VALUE_H
 #define VALUE_H
 
+<<<<<<< Updated upstream
 struct Value {
     uint8_t  size;
     uint64_t value;
+=======
+struct value {
+    uint8_t  size;
+    uint64_t value;
+    uint64_t highImpedance;
+>>>>>>> Stashed changes
     uint64_t unknown;
 };
 
