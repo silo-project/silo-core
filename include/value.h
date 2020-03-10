6f31f96b75b86dc8c0a09c6bfba0b1786466e842
LegenDUST <dhnam0502@naver.com>
2020-03-10 17:33:53 +0900

    용어 변경
    highImpedance가 unknown이기에 변경

--- include/value.h
+++ include/value.h
@@ -7,16 +7,16 @@ class Value {
 private: 
     uint8_t size;
     uint64_t value;
-    uint64_t highImpedance;
     uint64_t unknown;
+    uint64_t error;
 
 public:
-    Value(uint8_t size, uint64_t value, uint64_t highImpedance, uint64_t unknown) : size(size), value(value), highImpedance(highImpedance), unknown(unknown) { }
+    Value(uint8_t size, uint64_t value, uint64_t unknown, uint64_t error) : size(size), value(value), unknown(unknown), error(error) { }
 
     uint8_t getSize();
     uint64_t getValue();
-    uint64_t getHighImpedance();
     uint64_t getUnknown();
+    uint64_t getError();
 };
 
 #endif
