08b738c3d14ed744c6b4393c5f9939cf9d9d9c26
Jongeon <Jongeon@users.noreply.github.com>
2020-03-10 17:57:20 +0900

    사소한 것들 수정.

--- include/value.h
+++ include/value.h
@@ -5,18 +5,18 @@
 
 class Value {
 private: 
-    uint8_t size;
-    uint64_t value;
-    uint64_t unknown;
-    uint64_t error;
+  uint8_t size;
+  uint64_t value;
+  uint64_t unknown;
+  uint64_t error;
 
 public:
-    Value(uint8_t size, uint64_t value, uint64_t unknown, uint64_t error) : size(size), value(value), unknown(unknown), error(error) { }
+  Value(uint8_t size, uint64_t value, uint64_t unknown, uint64_t error) : size(size), value(value), unknown(unknown), error(error) { }
 
-    uint8_t getSize();
-    uint64_t getValue();
-    uint64_t getUnknown();
-    uint64_t getError();
+  uint8_t getSize();
+  uint64_t getValue();
+  uint64_t getUnknown();
+  uint64_t getError();
 };
 
 #endif
