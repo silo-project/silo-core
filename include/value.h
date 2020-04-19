9c533432385d951d3a4a4f5bed519fa218909787
Sangwon Ryu <bdboy090@naver.com>
2020-04-19 15:33:17 +0900

    Refactor value

--- include/value.h
+++ include/value.h
@@ -1,7 +1,9 @@
-#include <cstdint>
-
 #ifndef VALUE_H
 #define VALUE_H
+
+#include <cstdint>
+
+
 
 class Value {
 private:
@@ -11,16 +13,31 @@ private:
 
 public:
     Value(uint8_t width, uint64_t value, uint64_t state);
-
     Value(uint8_t width, uint64_t value, uint64_t unknown, uint64_t error);
 
-    inline uint8_t getWidth() const;
-    inline uint64_t getValue() const;
-    inline uint64_t getState() const;
+    inline uint8_t getWidth() const {
+        return this->width;
+    }
 
-    inline uint64_t getLogicalValue() const;
-    inline uint64_t getError() const;
-    inline uint64_t getUnknown() const;
+    inline uint64_t getRawValue() const {
+        return this->value;
+    }
+
+    inline uint64_t getState() const {
+        return this->state;
+    }
+
+    inline uint64_t getValue() const {
+        return this->value & ~this->state;
+    }
+
+    inline uint64_t getError() const {
+        return this->value & this->state;
+    }
+
+    inline uint64_t getUnknown() const {
+        return ~this->value & this->state;
+    }
 };
 
 #endif
--- src/value.cpp
+++ src/value.cpp
@@ -1,35 +1,11 @@
 #include "value.h"
 
-Value::Value(uint8_t width, uint64_t value, uint64_t state) 
-    : width(width), value(value), state(state) {
+
+
+Value::Value(uint8_t width, uint64_t raw_value, uint64_t state) : width(width), value(raw_value), state(state) {
 
 }
 
-Value::Value(uint8_t width, uint64_t value, uint64_t unknown, uint64_t error) 
-    : width(width), value(value | error), state(unknown | error) {
+Value::Value(uint8_t width, uint64_t value, uint64_t unknown, uint64_t error) : width(width), value(value | error), state(unknown | error) {
 
-}
-
-inline uint8_t Value::getWidth() const {
-    return this->width;
-}
-
-inline uint64_t Value::getValue() const {
-    return this->value;
-}
-
-inline uint64_t Value::getState() const {
-    return this->state;
-}
-
-inline uint64_t Value::getLogicalValue() const {
-    return this->value & ~this->state;
-}
-
-inline uint64_t Value::getError() const {
-    return this->value & this->state;
-}
-
-inline uint64_t Value::getUnknown() const {
-    return ~this->value & this->state;
 }
